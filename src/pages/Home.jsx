d0b88d58d2dea0d5c774ff6f6cf3df8b968b2f61
amithkashyap <amithkashyaprv@gmail.com>
2024-02-09 14:48:47 +0530

    added Home, Pricing and Product pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import PageNav from "../components/PageNav";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>Home</h1>
+    </div>
+  );
 };
 
 export default Home;
