9af742253b71a6853e0593dd5c44d03d69447b16
amithkashyap <amithkashyaprv@gmail.com>
2024-02-18 23:47:38 +0530

    implemented delete city feature

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -2,18 +2,30 @@ import React from "react";
 import styles from "./CityItem.module.css";
 import { formatDate } from "./City";
 import { Link } from "react-router-dom";
+import { useCities } from "../contexts/CitiesContext";
 
 const CityItem = ({ city }) => {
   const { emoji, cityName, date, id, position } = city;
+  const { currentCity, deleteCity } = useCities();
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    deleteCity(id);
+  };
+
   return (
     <Link
-      className={styles.cityItem}
+      className={`${styles.cityItem} ${
+        id === currentCity.id ? styles["cityItem--active"] : ""
+      }`}
       to={`${id}?lat=${position.lat}&lng=${position.lng}`}
     >
       <span className={styles.emoji}>{emoji}</span>
       <h3 className={styles.name}>{cityName}</h3>
       <time className={styles.date}>{formatDate(date)}</time>
-      <button className={styles.deleteBtn}>&times;</button>
+      <button className={styles.deleteBtn} onClick={handleDelete}>
+        &times;
+      </button>
     </Link>
   );
 };
