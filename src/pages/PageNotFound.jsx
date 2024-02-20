12ba7bf75b1e26c7bd3bca0d7c10f2ecca436bf4
amithkashyap <amithkashyaprv@gmail.com>
2024-02-20 23:53:23 +0530

    added PageNotFound component

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-
-const PageNotFound = () => {
-  return <div>PageNotFound</div>;
-};
-
-export default PageNotFound;
+export default function PageNotFound() {
+  return (
+    <main>
+      <h1>Page not found 😢</h1>
+    </main>
+  );
+}
